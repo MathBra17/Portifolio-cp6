52964702fc58cecd6e5aaaf009f0b1b81cc10f04
Matheus Brasil <matheusbrasilangelotti@gmail.com>
2025-11-05 23:03:50 -0300

    adicionando projetos no App.js

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,15 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import Contato from './Pages/Contato';
+import Projetos from './Pages/Projetos';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/contato" element={<Contato />} />
+      <Route path="/projeto" element={<Projetos />} />
     </Routes>
   );
 }
